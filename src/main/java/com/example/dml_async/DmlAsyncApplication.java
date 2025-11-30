2cef7437a8d9c2e71ffbb18494eef67e30c360df
seven <xodnr1531@gmail.com>
2025-11-30 10:36:00 +0900

    생성

---
+++ src/main/java/com/example/dml_async/DmlAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.dml_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmlAsyncApplication {
+//dsa das
+	public static void main(String[] args) {
+		SpringApplication.run(DmlAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dml_async/DmlAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dml_async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmlAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
